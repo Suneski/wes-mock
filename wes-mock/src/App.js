d528fbedeb57533a77a8581d71fcda8ac661b838
David Espinoza <despinoza1027@gmail.com>
2017-07-04 20:43:36 -0500

    added wes-mock folder, initial

---
+++ wes-mock/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <ol>
+            <li>Shop</li>
+            <li>FAQ</li>
+            <li>Shipping</li>
+            <li>About Us</li>
+            <li>Contact</li>
+          </ol>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
