b506b174d307a48b0b0255ec89945f371be78f8c
David Espinoza <despinoza1027@gmail.com>
2017-07-16 13:00:17 -0500

    added volleyballs to array

--- src/products.js
+++ src/products.js
@@ -1,8 +1,47 @@
 const products = [
   {
-    productName: .
-    productCode: ,
-    productDesc: ,
-    
+    category: "volleyball",
+    productName: "Wilson NCAA Official Sand Game Ball",
+    productDesc: "This is the current version of the official ball of NCAA Sand Volleyball.",
+    partNo: "WTH5100",
+    item: "Wilson Sand Volleyball",
+    price: "38.00",
+    image: "shop-images/NCAABall2016125px.jpg",
+  },
+  {
+    category: "volleyball",
+    productName: "Wilson AVP Official Game Ball",
+    productDesc: "The official game ball of the AVP. All the Armato balls are gone.",
+    partNo: "WTH4308",
+    item: "Wilson AVP Game Ball",
+    price: "40.00",
+    image: "shop-images/NEWAVP.jpg",
+  },
+  {
+    category: "volleyball",
+    productName: "Spalding USA King of the Beach Volleyball",
+    productDesc: "The official ball of USA Volleyball. It is the updated version of the old \"King of the Beach\" ball. It has an all-weather composite cover and a butyl bladder for improved air retention. We compared one of these to one of the Wilson AVP balls. We don't think you will notice enough difference in play that it would take an adjustment.",
+    partNo: "72-1138",
+    item: "Spalding USA Beach Volleyball - Composite",
+    price: "36.00",
+    image: "shop-images/SpaldingUSAKob.jpg",
+  },
+  {
+    category: "volleyball",
+    productName: "NVL-PRO - Mikasa NVL Official Game Ball",
+    productDesc: "The official game ball of the National Volleyball League. Sand not included.",
+    partNo: "72-1138",
+    item: "NVL-Pro Mikasa Official Game Ball",
+    price: "32.00",
+    image: "shop-images/NVLPRO.jpg",
+  },
+  {
+    category: "volleyball",
+    productName: "VLS300 - Offical Game Ball of 2012 Olympics",
+    productDesc: "The official beach ball of the 2012 Olympics. This ball has a new panel shape. If it is raining or wet, this ball is the one to use. It comes back from getting soaked better than any "real" volleyball we have seen.",
+    partNo: "VLS300",
+    item: "Mikasa Beach Champ VLS300",
+    price: "53.00",
+    image: "shop-images/VLS300.jpg",
   },
 ]
