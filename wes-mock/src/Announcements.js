80e31dbfdf7a3422223d6d7a155c9b6a42f460a2
David Espinoza <despinoza1027@gmail.com>
2017-07-15 21:59:58 -0500

    set up Announcements component

---
+++ wes-mock/src/Announcements.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <div id="announcements">
+        <p>This is an announcement</p>
+      </div>
+    )
+  }
+}
